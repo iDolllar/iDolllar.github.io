ef0d9da7468558932d4e097ce91c30b36d57de83
zogry aadith <zogryaadith007@outlook.com>
2022-01-28 13:39:56 -0500

    added ice-3

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,66 +1,148 @@
 // IIFE -- Immediately Invoked Function Expression
-// AKA Anonymous Self-Executing Function
+// AKA -- Anonymous Self-Executing Function
 (function()
 {
-
     function DisplayHomePage()
     {
+        console.log("Home Page");
         let AboutUsButton = document.getElementById("AboutUsButton");
-        
         AboutUsButton.addEventListener("click", function()
         {
-            // redirect to about page
-            location.href = "aboutus.html";
+            location.href = "about.html";
         });
 
-        // Step 1 get a reference to an entry point(s) (insertion point / deletion point)
-        let MainContent = document.getElementsByTagName("main")[0];
+        // Step 1. get an entry point(s) (insertion point / deletion point) reference 
         let DocumentBody = document.body;
-        
-        // Step 2 create an element(s) to insert
+        let MainContent = document.getElementsByTagName("main")[0];
+
+        // Step 2. create an element(s) to insert
         let MainParagraph = document.createElement("p");
         let Article = document.createElement("article");
-        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
+        let ArticleParagraph = `<p id="ArticleParagraph" class ="mt-3">This is the Article Paragraph</p>`
 
-        // Step 3 configure new element
+        // Step 3. configure the new element
         MainParagraph.setAttribute("id", "MainParagraph");
         MainParagraph.setAttribute("class", "mt-3");
-
-        let FirstParagraphString = "This is";
-        // example of Template String
-        let SecondParagraphString = `${FirstParagraphString} the Main Paragraph`;
-
-        MainParagraph.textContent = SecondParagraphString;
+        MainParagraph.textContent = "This is the Main Paragraph";
         Article.setAttribute("class", "container");
 
-        // Step 4 add / insert new element
+        // Step 4. Add / Insert the new element
         MainContent.appendChild(MainParagraph);
         Article.innerHTML = ArticleParagraph;
         DocumentBody.appendChild(Article);
 
+        // to insert before
+        // target a location (element) to insert before 
+        // TargetElement.before(elementToInsert);
 
-        // Deletion example
-        //document.getElementById("ArticleParagraph").remove();
-
-        // Insert Before example
-        // let NewH1 = document.createElement("h1");
-        // NewH1.setAttribute("class", "display-1");
-        // NewH1.textContent = "Hello, World!";
-        // MainContent.before(NewH1);
-
+        // to delete
+        //Article.remove();
     }
 
-    // named function
+    function DisplayProductsPage()
+    {
+        console.log("Products Page");
+    }
+
+    function DisplayServicesPage()
+    {
+        console.log("Services Page");
+    }
+
+    function DisplayAboutPage()
+    {
+        console.log("About Page");
+    }
+
+    function DisplayContactPage()
+    {
+        console.log("Contact Page");
+
+        let sendButton = document.getElementById("sendButton");
+        let subscribeCheckbox = document.getElementById("subscribeCheckbox");
+
+        sendButton.addEventListener("click", function(event)
+        {
+            //event.preventDefault(); // right now for testing only
+
+            if(subscribeCheckbox.checked)
+            {
+                let contact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if(contact.serialize())
+                {
+                    let key = contact.FullName.substring(0, 1) + Date.now();
+
+                    localStorage.setItem(key, contact.serialize());
+                }
+            }
+        });
+    }
+
+    function DisplayContactListPage()
+    {
+        if(localStorage.length > 0)
+        {
+            let contactList = document.getElementById("contactList");
+
+            let data = "";
+
+            let keys = Object.keys(localStorage); // returns a list of keys from localStorage
+
+            let index = 1;
+
+            // for every key in the keys string array
+            for(const key of keys)
+            {
+                let contactData = localStorage.getItem(key); // get localStorage data value
+
+                let contact = new Contact(); // create an empty Contact object
+                contact.deserialize(contactData);
+
+                data += `<tr>
+                <th scope="row" class="text-center">${index}</th>
+                <td>${contact.FullName}</td>
+                <td>${contact.ContactNumber}</td>
+                <td>${contact.EmailAddress}</td>
+                <td></td>
+                <td></td>
+                </tr>`;
+                
+                index++;
+            }
+
+            contactList.innerHTML = data;
+        }
+    }
+
+
+    // named function option
     function Start()
     {
-        console.log("App Started!!");
+        console.log("App Started!");
 
         switch(document.title)
         {
             case "Home":
                 DisplayHomePage();
                 break;
+            case "Our Products":
+                DisplayProductsPage();
+                break;
+            case "Our Services":
+                DisplayServicesPage();
+                break;
+            case "About Us":
+                DisplayAboutPage();
+                break;
+            case "Contact Us":
+                DisplayContactPage();
+                break;
+            case "Contact-List":
+                DisplayContactListPage();
+                break;
+
         }
+       
     }
 
     window.addEventListener("load", Start);
