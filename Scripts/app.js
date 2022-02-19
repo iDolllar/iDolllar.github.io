c86f341f122ed43af7f6b6db36e05f2bf72710a8
iDolllar <zogryaadith007@gmail.com>
2022-02-19 08:39:04 -0500

    Ice-6 on live website

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,6 +2,46 @@
 // AKA Anonymous Self-Executing Function
 (function()
 {
+    /**
+     * This method uses AJAX to open a connection to the url and returns data to the callback function
+     *
+     * @param {string} method
+     * @param {string} url
+     * @param {Function} callback
+     */
+    function AjaxRequest(method, url, callback)
+    {
+        // step 1 - instantiate an XHR object
+        let XHR = new XMLHttpRequest();
+
+        // step 2 - create an event listener / handler for readystatechange event
+        XHR.addEventListener("readystatechange", () =>
+        {
+            if(XHR.readyState === 4 && XHR.status === 200)
+            {
+               callback(XHR.responseText);
+            }
+        });
+
+        // step 3 - open a connection to the server
+        XHR.open(method, url);
+
+        // step 4 - send the request to the server
+        XHR.send();
+    }
+
+    /**
+     * This function loads the NavBar from the header file and injects it into the page
+     *
+     * @param {string} data
+     */
+    function LoadHeader(data)
+    {
+        $("header").html(data); // data payload
+        $(`li>a:contains(${document.title})`).addClass("active"); // add a class of 'active'
+        CheckLogin();
+    }
+
 
     function DisplayAboutPage()
     {
@@ -44,7 +84,7 @@
      */
     function AddContact(fullName, contactNumber, emailAddress)
     {
-        let contact = new Contact(fullName, contactNumber, emailAddress);
+        let contact = new core.Contact(fullName, contactNumber, emailAddress);
         if(contact.serialize())
         {
             let key = contact.FullName.substring(0, 1) + Date.now();
@@ -53,10 +93,46 @@
         }
     }
 
+    /**
+     * This method validates an input text field in the form and displays
+     * an error in the message area div element
+     *
+     * @param {string} input_field_ID
+     * @param {RegExp} regular_expression
+     * @param {string} error_message
+     */
+    function ValidateField(input_field_ID, regular_expression, error_message)
+    {
+        let messageArea = $("#messageArea").hide();
+
+        $("#" + input_field_ID).on("blur", function()
+        {
+            let input_text_field = $(this).val(); 
+            if(!regular_expression.test(input_text_field)) 
+            {
+                $(this).trigger("focus").trigger("select"); 
+                messageArea.addClass("alert alert-danger").text(error_message).show();
+            }
+            else 
+            {
+                messageArea.removeAttr("class").hide();
+            } 
+        });
+    }
+
+    function ContactFormValidation()
+    {
+        ValidateField("fullName",/^([A-Z][a-z]{1,3}.?\s)?([A-Z][a-z]{1,25})+(\s|,|-)([A-Z][a-z]{1,25})+(\s|,|-)*$/,"Please enter a valid Full Name. This must include at least a Capitalized first name followed by a Capitalized last Name.");
+        ValidateField("contactNumber",/^(\+\d{1,3}[\s-.])?\(?\d{3}\)?[\s-.]?\d{3}[\s-.]?\d{4}$/,"Please enter a valid Contact Number. Example: (905) 555-5555");
+        ValidateField("emailAddress",/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10}$/,"Please enter a valid Email Address.");
+    }
+
     function DisplayContactPage()
     {
         console.log("Contact Us Page");
 
+        ContactFormValidation();
+        
         let sendButton = document.getElementById("sendButton");
         let subscribeCheckbox = document.getElementById("subscribeCheckbox");
 
@@ -90,7 +166,7 @@
             {
                 let contactData = localStorage.getItem(key); // retrieve contact data from localStorage
 
-                let contact = new Contact(); // create an empty Contact Object
+                let contact = new core.Contact(); // create an empty Contact Object
                 contact.deserialize(contactData);
 
                 data += `<tr>
@@ -136,6 +212,8 @@
     {
         console.log("Edit Page");
 
+        ContactFormValidation();
+
         let page = location.hash.substring(1);
 
         switch(page)
@@ -165,7 +243,7 @@
             default:
                 {
                     // get the contact info from localStorage
-                    let contact = new Contact();
+                    let contact = new core.Contact();
                     contact.deserialize(localStorage.getItem(page));
 
                     // display the contact info in the edit form
@@ -200,13 +278,105 @@
         }
     }
 
+    function DisplayLoginPage()
+    {
+        console.log("Login Page");
+
+        let messageArea = $("#messageArea");
+        messageArea.hide();
+
+        $("#loginButton").on("click", function()
+        {
+            let success = false;
+
+            // create an empty User object
+            let newUser = new core.User();
+
+            // use jQuery shortcut to load the users.json file
+            $.get("./Data/users.json", function(data)
+            { 
+                // for everry user in the users.json file, loop
+                for (const user of data.users) 
+                {
+                    // check if the username and password entered match with user
+                    if(username.value == user.Username && password.value == user.Password)
+                    {
+                        // get the user data from the file and assign it to our empty user object
+                        newUser.fromJSON(user);
+                        success = true;
+                        break;
+                    }
+                }
+
+                 // if username and password matches - success...perform the login sequence
+                if(success)
+                {
+                    // add user to session storage
+                    sessionStorage.setItem("user", newUser.serialize());
+
+                    // hide any error messages
+                    messageArea.removeAttr("class").hide();
+
+                    // redirect the user to the secure area of our site - contact-list
+                    location.href = "contact-list.html";
+                }
+                else
+                {
+                    // display an error message
+                    $("#username").trigger("focus").trigger("select");
+                    messageArea.addClass("alert alert-danger").text("Error: Invalid Login Information.").show();
+                }
+            });
+
+           
+
+            $("#cancelButtton").on("click", function()
+            {
+                // clear the login form
+                document.forms[0].reset();
+
+                // return to  the home page
+                location.href = "index.html";
+            });
+        });
+    }
+
+    function CheckLogin()
+    {
+        // if user is logged in
+        if(sessionStorage.getItem("user"))
+        {
+            // swap out the login link for the logout link
+            $("#login").html(
+                `<a id="logout" class="nav-link" href="#"><i class="fas fa-sign-out-alt"></i> Logout</a>`
+            );
+
+            $("#logout").on("click", function()
+            {
+                // perform logout
+                sessionStorage.clear();
+
+                // redirect back to login
+                location.href = "login.html";
+            });
+        }
+    }
+
+    function DisplayRegisterPage()
+    {
+        console.log("Register Page");
+    }
+
     // named function
     function Start()
     {
         console.log("App Started!!");
 
-        switch (document.title) 
-        {
+        AjaxRequest("GET", "header.html", LoadHeader);
+
+        
+
+        switch (document.title) {
           case "Home":
             DisplayHomePage();
             break;
@@ -225,10 +395,15 @@
           case "Our Services":
             DisplayServicesPage();
             break;
-            case "Edit":
+          case "Edit":
             DisplayEditPage();
             break;
-
+          case "Login":
+            DisplayLoginPage();
+            break;
+          case "Register":
+            DisplayRegisterPage();
+            break;
         }
     }
 
